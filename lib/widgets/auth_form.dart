2d2ce5f93a9c8e949d8074ea1df315bb87c6ace8
Beren Erchamion <beren@houseofbeor.net>
2021-09-19 14:08:40 -0400

    FIlling out the auth screen and validators.

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -8,6 +8,21 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+
+    FocusScope.of(context).unfocus();
+    
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,25 +32,59 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16.0),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Email Address',
                     ),
+                    onSaved: (value){
+                      _userEmail = value!;
+                    },
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value == null || value.length < 8) {
+                        return 'Come on enter a reasonable username.';
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(
                       labelText: 'Username',
                     ),
+                    onSaved: (value) {
+                      _userName = value!;
+                    },
                   ),
                   TextFormField(
+                    validator: (value) {
+                      String pattern =
+                          r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+                      RegExp regex = new RegExp(pattern);
+                      if (value == null || value.length < 8) {
+                        return 'That password is not good at all.';
+                      }
+                      if (!regex.hasMatch(pattern)) {
+                        return 'That password is not complex enough.';
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(
                       labelText: 'Password',
                     ),
                     obscureText: true,
+                    onSaved: (value) {
+                      _userPassword = value!;
+                    },
                   ),
                   SizedBox(
                     height: 12,
